c2fc161ef70985067e9527367ccfe62ace0d3efc
Victor Miller <victor.miller@me.com>
2022-11-30 20:24:25 +0100

    Solved 2021 01 Part 1

--- 2021/js/01/solver.js
+++ 2021/js/01/solver.js
@@ -1,5 +1,19 @@
-import fs from 'fs';
+import fs from "fs/promises";
 
-fs.readFile('sample.txt', 'utf8', (err, data) => {
-  console.log(data)
-})
+async function readArr(fileName) {
+  const data = await fs.readFile(fileName, "utf8");
+  const arr = data.split(/\r?\n/);
+  return arr.map((i) => Number(i));
+}
+
+const data = await readArr("input.txt");
+
+const answer = data.filter((a, i) => {
+  if (i > 0) {
+    return a > data[i - 1];
+  } else {
+    false;
+  }
+});
+
+console.log(`The answer is: ${answer.length}`);
