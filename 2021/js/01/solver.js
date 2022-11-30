355b99af056ec03f687d27aede332f1cf1afafb0
Victor Miller <victor.miller@me.com>
2022-11-30 20:47:50 +0100

    Solved 2021 01 part 2

--- 2021/js/01/solver.js
+++ 2021/js/01/solver.js
@@ -6,14 +6,23 @@ async function readArr(fileName) {
   return arr.map((i) => Number(i));
 }
 
-const data = await readArr("input.txt");
+const data1 = await readArr("input-1.txt");
 
-const answer = data.filter((a, i) => {
-  if (i > 0) {
-    return a > data[i - 1];
-  } else {
-    false;
-  }
-});
+function solver(data) {
+  return data.filter((a, i) => {
+    if (i > 0) {
+      return a > data[i - 1];
+    } else {
+      false;
+    }
+  });
+}
 
-console.log(`The answer is: ${answer.length}`);
+const data2 = await readArr("input-2.txt");
+const tripples = [];
+for (let i = 0; i < data2.length - 2; i++) {
+  tripples.push(data2[i] + data2[i + 1] + data2[i + 2]);
+}
+
+console.log(`The answer part 1 is: ${solver(data1).length}`);
+console.log(`The answer part 2 is: ${solver(tripples).length}`);
