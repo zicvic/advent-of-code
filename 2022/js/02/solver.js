90732bf59de7577b4e56ce52f44d6f03fc4cbc24
Victor Miller <victor.miller@me.com>
2022-12-03 13:16:45 +0100

    2022 02 part 2

--- 2022/js/02/solver.js
+++ 2022/js/02/solver.js
@@ -1,5 +1,8 @@
 import fs from "fs/promises";
 
+const WEAPONS = ["A", "B", "C"];
+const STRATEGIES = ["X", "Y", "Z"];
+
 const SCORE = {
   win: 6,
   draw: 3,
@@ -12,13 +15,25 @@ async function readColumns(fileName) {
   return arr.map((i) => i.split(/\s/));
 }
 
-const data = await readColumns("input-1.txt");
+const getChoiceFromStupidStrategy = (opponent, strat) => {
+  return WEAPONS[STRATEGIES.indexOf(strat)];
+};
 
-const rockPaperScissors = (opponent, you) => {
-  const OPPONENT = ["A", "B", "C"];
-  const YOU = ["X", "Y", "Z"];
-  const oIndex = OPPONENT.indexOf(opponent);
-  const uIndex = YOU.indexOf(you);
+const getChoiceFromStrategy = (opponent, strat) => {
+  const oIndex = WEAPONS.indexOf(opponent);
+  const sIndex = STRATEGIES.indexOf(strat);
+  let choice;
+  if (sIndex === 1) {
+    choice = oIndex;
+  } else {
+    choice = sIndex === 2 ? (oIndex + 1) % 3 : (oIndex + 2) % 3;
+  }
+  return WEAPONS[choice];
+};
+
+const rockPaperScissors = (opponent, you, strategyFunction) => {
+  const oIndex = WEAPONS.indexOf(opponent);
+  const uIndex = WEAPONS.indexOf(strategyFunction(opponent, you));
   let score;
   if (oIndex !== uIndex) {
     score = oIndex === (uIndex + 1) % 3 ? SCORE.loss : SCORE.win;
@@ -28,7 +43,17 @@ const rockPaperScissors = (opponent, you) => {
   return score + uIndex + 1;
 };
 
-const score = data
-  .map((fight) => rockPaperScissors(...fight))
+const data1 = await readColumns("input-1.txt");
+const data2 = await readColumns("input-2.txt");
+
+const scorePart1 = data1
+  .map((fight) => rockPaperScissors(...fight, getChoiceFromStupidStrategy))
   .reduce((acc, curr) => acc + curr, 0);
-console.log(score);
+
+console.log(`Score Part 1: ${scorePart1}`);
+
+const scorePart2 = data2
+  .map((fight) => rockPaperScissors(...fight, getChoiceFromStrategy))
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log(`Score Part 2: ${scorePart2}`);
