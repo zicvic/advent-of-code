c7d658ea3d78ae020105f1b0dc7d30edd2615568
°Victor Miller <victor.miller@me.com>
2024-12-13 23:43:52 +0100

    Solved 2024/13 Part 1

--- 2024/py/13/solver.py
+++ 2024/py/13/solver.py
@@ -1,9 +1,14 @@
 import numpy as np
 
+def isinteger(x):
+    return np.abs(x - np.round(x)) < 0.001 
+
 def read_input_file(file_path):
+    As = []
+    Ys = []
     A = []
     Y = []
-    
+
     with open(file_path, 'r') as file:
         for line in file:
             if line.startswith("Button A:") or line.startswith("Button B:"):
@@ -16,24 +21,36 @@ def read_input_file(file_path):
                 x = int(parts[0].replace("X=", ""))
                 y = int(parts[1].replace("Y=", ""))
                 Y = [x, y]
+                
+                A = np.array(A).transpose()
+                Y = np.array(Y).reshape(-1, 1)
+                As.append(A)
+                Ys.append(Y)    
+                A = []
+                Y = []
     
-    A = np.array(A)
-    Y = np.array(Y).reshape(-1, 1)
-    
-    return A, Y
+    return As, Ys
 
 def solve_for_x(A, Y):
     try:
         A_inv = np.linalg.inv(A)
         x = np.dot(A_inv, Y)
-        return x
+        if np.all((x >= 0) & (x <= 100)) and np.all(isinteger(x)):
+            return x
+        else:
+            return f'Invalid range of x= ${x}'
     except np.linalg.LinAlgError:
         return "Matrix A is singular and cannot be inverted."
 
-# Example usage
-A, Y = read_input_file('input_1.txt')
-print("A =", A)
-print("Y =", Y)
-x = solve_for_x(A, Y)
-print("x =", x)
+
+
+prize = np.array([3, 1])
+cost = 0
+As, Ys = read_input_file('input.txt')
+for A, Y in zip(As, Ys):
+    x = solve_for_x(A, Y)
+    if type(x) != str:
+        cost = cost + np.dot(prize, x)
+
+print(f'Total cost: ${cost}')
 
