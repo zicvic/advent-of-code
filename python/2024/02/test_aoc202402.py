4c48b2af9c9d141d2ca7dab53936176dd20a855d
°Victor Miller <victor.miller@me.com>
2024-12-16 21:14:20 +0100

    Added 2024/02

---
+++ python/2024/02/test_aoc202402.py
@@ -0,0 +1,46 @@
+"""Tests for AoC 2, 2024."""
+
+# Standard library imports
+import pathlib
+
+# Third party imports
+import aoc202402
+import pytest
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().rstrip()
+    return aoc202402.parse_data(puzzle_input)
+
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().rstrip()
+    return aoc202402.parse_data(puzzle_input)
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    assert example1 == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part1_example1(example1):
+    """Test part 1 on example input."""
+    assert aoc202402.part1(example1) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example1(example1):
+    """Test part 2 on example input."""
+    assert aoc202402.part2(example1) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example input."""
+    assert aoc202402.part2(example2) == ...
